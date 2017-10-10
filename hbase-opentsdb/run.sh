d230cb569240f42f471b76cd1d6bd8ec5595ddc7
Fabrizio Sandri <fsandri00@gmail.com>
2017-10-10 19:20:32 +0000

    OpenTSDB - Grafana docker-compose

---
+++ hbase-opentsdb/run.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+echo "Starting hbase"
+/./opt/hbase-1.2.6/bin/start-hbase.sh
+
+if [ ! -e /opt/tables ] ; then
+    env COMPRESSION=NONE HBASE_HOME=/opt/hbase-1.2.6/ /opt/opentsdb-2.3.0/src/create_table.sh
+    touch /opt/tables
+fi;
+
+if [ ! -e /opt/data-source ] ; then
+    curl 'http://admin:admin@grafana:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"localOpenTSDB","type":"opentsdb","url":"http://hbase-opentsdb:4242","access":"proxy","isDefault":true}'
+    touch /opt/data-source
+fi;
+
+echo "Starting OpenTSDB"
+/./opt/opentsdb-2.3.0/build/tsdb tsd --port=4242 --zkquorum=zookeeper --staticroot=/opt/opentsdb-2.3.0/build/staticroot --config=/opt/opentsdb-2.3.0/src/opentsdb.conf
